99c43911efc5956b78c482a1056d62586a96b7f0
Fabian Sperrle <fabian.sperrle@uni.kn>
2015-06-11 10:37:59 +0200

    Use thumb() for menu background images

--- site/snippets/menu.php
+++ site/snippets/menu.php
@@ -1,5 +1,5 @@
 <nav role="navigation">
-    <div class="row navigation" style="background: url('<?= $site->images()->shuffle()->first()->url() ?>'); background-size: cover;">
+    <div class="row navigation" style="background: url('<?= thumb($site->images()->shuffle()->first(), array('width' => 1000, 'upscale' => false, 'quality' => 80))->url() ?>'); background-size: cover;">
         <div class="small-7 medium-4 large-3 columns">
             <?php foreach ($site->pages()->visible() as $page): ?>
                 <a href="<?= $page->url() ?>" alt="<?= $page->title() ?>">
