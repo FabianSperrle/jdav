51cb582b87c70078ea3e38ab738ea73a5916aa6d
Fabian Sperrle <fabian.sperrle@uni.kn>
2015-09-16 14:19:01 +0200

    Accept groups without image + copy/paste cleanup

--- site/templates/jugendgruppen.php
+++ site/templates/jugendgruppen.php
@@ -1,76 +1,35 @@
 <?php snippet('header') ?>
 
-<div class="row">
-    <div class="small-5 columns">
-        <hr/>
-    </div>
-    <div class="small-2 columns"><h4>Radolfzell</h4></div>
-    <div class="small-5 columns">
-        <hr/>
-    </div>
-    <div class="small-12 columns">
-        <ul class="small-block-grid-3">
-            <?php foreach ($page->children()->visible()->filterBy('ort', '==', 'radolfzell')->sortBy('name', 'ASC') as $group): ?>
-                <li>
-                    <a href="<?= $group->url() ?>"><img
-                            src="<?php echo thumb($group->images($group->bild()->text()), array('width' => 300, 'upscale' => true))->url() ?>"/></a>
+<?php $locations = ['Radolfzell', 'Konstanz', 'Singen'];
+foreach ($locations as $location): ?>
+    <div class="row">
+        <div class="small-5 columns">
+            <hr/>
+        </div>
+        <div class="small-2 columns"><h4><?= $location ?></h4></div>
+        <div class="small-5 columns">
+            <hr/>
+        </div>
+        <div class="small-12 columns">
+            <ul class="small-block-grid-3">
+                <?php foreach ($page->children()->visible()->filterBy('ort', '==', strtolower($location))->sortBy('name', 'ASC') as $group): ?>
+                    <li>
+                        <?php 
+                        $image;
+                        if (!$group->bild()->isEmpty()) {
+                            $image = $group->images($group->bild());
+                        } else {
+                            $image = $page->images('nophoto.jpg');
+                        } ?>
+                        <a href="<?= $group->url() ?>"><img
+                                src="<?php echo thumb($image, array('width' => 300, 'upscale' => true))->url() ?>"/></a>
 
-                    <div class="text-center"><?= $group->title() ?></div>
-                </li>
-            <?php endforeach; ?>
-        </ul>
+                        <div class="text-center"><?= $group->title() ?></div>
+                    </li>
+                <?php endforeach; ?>
+            </ul>
+        </div>
     </div>
-</div>
-
-<div class="row">
-    <div class="small-5 columns">
-        <hr/>
-    </div>
-    <div class="small-2 columns"><h4>Konstanz</h4></div>
-    <div class="small-5 columns">
-        <hr/>
-    </div>
-    <div class="small-12 columns">
-        <ul class="small-block-grid-3">
-            <?php foreach ($page->children()->visible()->filterBy('ort', '==', 'konstanz')->sortBy('name', 'ASC') as $group): ?>
-                <li>
-                    <?php 
-                    $image;
-                    if ($group->bild() != "") {
-                        $image = $group->images($group->bild());
-                    } else {
-                        $image = $page->images('nophoto.jpg');
-                    } ?>
-                    <a href="<?= $group->url() ?>"><img
-                            src="<?php echo thumb($image, array('width' => 300, 'upscale' => true))->url() ?>"/></a>
-
-                    <div class="text-center"><?= $group->title() ?></div>
-                </li>
-            <?php endforeach; ?>
-        </ul>
-    </div>
-</div>
-
-<div class="row">
-    <div class="small-5 columns">
-        <hr/>
-    </div>
-    <div class="small-2 columns"><h4>Singen</h4></div>
-    <div class="small-5 columns">
-        <hr/>
-    </div>
-    <div class="small-12 columns">
-        <ul class="small-block-grid-3">
-            <?php foreach ($page->children()->visible()->filterBy('ort', '==', 'singen')->sortBy('name', 'ASC') as $group): ?>
-                <li>
-                    <a href="<?= $group->url() ?>"><img
-                            src="<?php echo thumb($group->images($group->bild()->text()), array('width' => 300, 'upscale' => true))->url() ?>"/></a>
-
-                    <div class="text-center"><?= $group->title() ?></div>
-                </li>
-            <?php endforeach; ?>
-        </ul>
-    </div>
-</div>
+<?php endforeach; ?>
 
 <?php snippet('footer') ?>
